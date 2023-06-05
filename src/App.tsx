82d01cd80c37c7744840335afda8153757c9466f
Aswini Kumar Rath <aswinirath.sp@gmail.com>
2023-06-05 23:03:01 +0530

    random_user

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+interface User {
+  name: {
+    first: string;
+    last: string;
+  };
+  email: string;
+}
+
+const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const fetchRandomUser = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api');
+      const userData: User = {
+        name: {
+          first: response.data.results[0].name.first,
+          last: response.data.results[0].name.last
+        },
+        email: response.data.results[0].email
+      };
+      setUser(userData);
+      localStorage.setItem('randomUser', JSON.stringify(userData));
+    } catch (error) {
+      console.error('Error fetching random user:', error);
+    }
+  };
+
+  useEffect(() => {
+    const cachedUser = localStorage.getItem('randomUser');
+    if (cachedUser) {
+      setUser(JSON.parse(cachedUser));
+    } else {
+      fetchRandomUser();
+    }
+  }, []);
+
+  const handleRefresh = () => {
+    fetchRandomUser();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random User</h1>
+      {user ? (
+        <div>
+          <p>Name: {user.name.first} {user.name.last}</p>
+          <p>Email: {user.email}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button onClick={handleRefresh}>Refresh</button>
     </div>
   );
-}
+};
 
 export default App;
